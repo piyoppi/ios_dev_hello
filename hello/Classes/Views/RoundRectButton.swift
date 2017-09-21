a69f354c9d7d4e9e2a9888f93e63304ad1be5d4f
Shohei Ogata <shohei.ogata@pepabo.com>
2017-09-21 15:17:30 +0900

    カスタムデザインなボタンを設置した

--- hello/Classes/Views/RoundRectButton.swift
+++ hello/Classes/Views/RoundRectButton.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class RoundRectButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable
+    var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
     }
-    */
-
+    
+    @IBInspectable
+    var buttonColor: UIColor = UIColor.white {
+        didSet {
+            self.backgroundColor = buttonColor
+        }
+    }
+    
+    @IBInspectable
+    var textColor: UIColor = UIColor.white {
+        didSet {
+            self.setTitleColor(textColor, for: .normal)
+        }
+    }
 }
